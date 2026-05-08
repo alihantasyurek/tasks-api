fab11aa51c785c945ca052422aa9f7d4a01a0978
Via <alihantasyurek@gmail.com>
2026-05-08 14:06:25 +0300

    feat: add auth routes

--- server.js
+++ server.js
@@ -5,5 +5,7 @@ const setupSwagger = require("./config/swagger.js");
 const app = express();
 setupSwagger(app);
 
+app.use("/auth", require("./routes/auth.js"));
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
